01a7cecf171a3ee84f215944e96552668826dfb1
Sebastian Kumór <sebastian_k86@o2.pl>
2019-07-10 13:22:05 +0200

    Dodaj pliki projektów.

---
+++ WCFConsole/FirstWcfcs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FirstWcfcs : IMyWcf
+{
+    public string Hello()
+    {
+        return "First work";
+    }
+
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
+
---
+++ WCFConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Uri adres = new Uri("http://localhost:2222/Hello");
+
+           ServiceHost host = new ServiceHost(typeof(FirstWcfcs), adres);
+
+           host.AddServiceEndpoint(typeof(IMyWcf), new BasicHttpBinding(), adres);
+           host.Open();
+           Console.WriteLine("Serwer uruchomiony");
+           Console.ReadLine();
+
+        }
+    }
+
